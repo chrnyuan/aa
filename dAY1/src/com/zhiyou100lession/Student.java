fbe0cda2ee025cf09b3ee9fa5c0e4233d3517060
runoob <test@runoob.com>
2018-12-19 15:21:46 +0800

    瞬时

---
+++ dAY1/src/com/zhiyou100lession/Student.java
@@ -0,0 +1,9 @@
+package com.zhiyou100lession;
+
+	public class Student{
+		
+		String name;
+		
+	}
+	
+	
---
+++ dAY1/src/com/zhiyou100lession/ss.java
@@ -0,0 +1,11 @@
+package com.zhiyou100lession;
+
+
+
+public class ss extends Student{
+
+	public void seting(){
+		String aString = super.name;
+	}
+
+}
